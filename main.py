1c3744cde9f32c4351ada61688f1e891d3f48853
Jeisonsuarezcol <jeisonsuarezcol@gmail.com>
2023-07-06 21:05:55 -0500

    decimo commt 06/07

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ df_production_companies = pd.read_csv('datasets/production_companies.csv')
 df_crew = pd.read_csv('datasets/data_crew.csv')
 
 df = df_final[['title']]
-df = df[:4000]
+df = df[:8000]
 
 
 @app.get('/')
