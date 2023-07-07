6125490d4088e78f75ed634e4b998ea8b465cb77
Jeisonsuarezcol <jeisonsuarezcol@gmail.com>
2023-07-06 19:37:03 -0500

    quinto commit 06/07

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ df_production_companies = pd.read_csv('datasets/production_companies.csv')
 df_crew = pd.read_csv('datasets/data_crew.csv')
 
 df = df_final[['title']]
-df = df[:27000]
+df = df[:20000]
 
 
 @app.get('/')
